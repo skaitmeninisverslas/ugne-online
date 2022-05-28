f6848263245b392348ac4b9708b6d8ca2b011b44
Mantas <skaitmeninisverslas@gmail.com>
2022-05-28 09:53:29 +0300

    ready to heroku

--- index.js
+++ index.js
@@ -50,6 +50,6 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
-app.listen(4000, () => {
-  console.log("App listening on port 4000");
+app.listen(process.env.PORT || 4000, () => {
+  console.log(`App listening on port ${process.env.PORT}`);
 });
