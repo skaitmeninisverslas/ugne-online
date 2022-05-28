5a31ab464f18b96ae7ad58bf5b21d03f97f531f5
Mantas <skaitmeninisverslas@gmail.com>
2022-05-28 10:06:07 +0300

    heroku deploy

--- middleware/storePost.js
+++ middleware/storePost.js
@@ -1,8 +1,13 @@
 module.exports = (req, res, next) => {
-
-  if (!req.files.image || !req.body.category || !req.body.title || !req.body.description || !req.body.content) {
-      return res.redirect('/admin')
+  if (
+    !req.files.image ||
+    !req.body.category ||
+    !req.body.title ||
+    !req.body.description ||
+    !req.body.content
+  ) {
+    return res.redirect("/admin");
   }
 
-  next()
-}
+  next();
+};
