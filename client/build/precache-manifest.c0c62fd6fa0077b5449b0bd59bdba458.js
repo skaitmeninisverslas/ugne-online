4bb6cfe9c501d0c18df75e03ae12d97ef545c117
Mantas Jakevicius <mantas.jakevicius@scale3c.com>
2022-09-26 21:40:23 +0300

    FIxes

---
+++ client/build/precache-manifest.c0c62fd6fa0077b5449b0bd59bdba458.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3fe7ea2f9adf4a31d805406916160b1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "595868faac690217b00c",
+    "url": "/static/css/main.bddff954.chunk.css"
+  },
+  {
+    "revision": "44d3b3e5d0cc5d828cec",
+    "url": "/static/js/2.57e66e5e.chunk.js"
+  },
+  {
+    "revision": "5d404678f3744ea582e1f9f8fe795530",
+    "url": "/static/js/2.57e66e5e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "595868faac690217b00c",
+    "url": "/static/js/main.d993334d.chunk.js"
+  },
+  {
+    "revision": "0281e3aa3ea9fc02c242",
+    "url": "/static/js/runtime-main.b12ff581.js"
+  },
+  {
+    "revision": "73365842c2ace090b884417972f7e8e6",
+    "url": "/static/media/LTCBodoni175W01-Regular.73365842.woff"
+  },
+  {
+    "revision": "92883b3300f3f4b8be5e4a2c6a5614a9",
+    "url": "/static/media/LTCBodoni175W01-Regular.92883b33.woff2"
+  },
+  {
+    "revision": "088a34f78f530102fd9661173b4a4f26",
+    "url": "/static/media/fa-brands-400.088a34f7.eot"
+  },
+  {
+    "revision": "273dc9bf9778fd37fa61357645d46a28",
+    "url": "/static/media/fa-brands-400.273dc9bf.ttf"
+  },
+  {
+    "revision": "822d94f19fe57477865209e1242a3c63",
+    "url": "/static/media/fa-brands-400.822d94f1.woff2"
+  },
+  {
+    "revision": "d72293118cda50ec50c39957d9d836d0",
+    "url": "/static/media/fa-brands-400.d7229311.svg"
+  },
+  {
+    "revision": "f4920c94c0861c537f72ba36590f6362",
+    "url": "/static/media/fa-brands-400.f4920c94.woff"
+  },
+  {
+    "revision": "2aa6edf8f296a43b32df35f330b7c81c",
+    "url": "/static/media/fa-solid-900.2aa6edf8.ttf"
+  },
+  {
+    "revision": "7a5de9b08012e4da40504f2cf126a351",
+    "url": "/static/media/fa-solid-900.7a5de9b0.svg"
+  },
+  {
+    "revision": "7fb1cdd9c3b889161216a13267b55fe2",
+    "url": "/static/media/fa-solid-900.7fb1cdd9.eot"
+  },
+  {
+    "revision": "93f284548b42ab76fe3fd03a9d3a2180",
+    "url": "/static/media/fa-solid-900.93f28454.woff"
+  },
+  {
+    "revision": "f6121be597a72928f54e7ab5b95512a1",
+    "url": "/static/media/fa-solid-900.f6121be5.woff2"
+  }
+]);
